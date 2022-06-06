18f325fa344a3d084cbaa0edfdaa2795dc8319d0
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-06 19:04:11 -0300

    Adiciona requisito 1.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,1 @@
-// Iniciando o projeto
 test('', () => {});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+import userEvent from '@testing-library/user-event';
+
+describe('Verifica o componente App.js', () => {
+  it('Verifica se existe um link Home e se redireciona para "/" ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se existe um link About e se redireciona para "/about" ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica se existe Favorite Pokémons e se redireciona para "/favorites" ', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorite).toBeInTheDocument();
+
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
