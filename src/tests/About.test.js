a43a60b19bc25dd2f5d2925eee97178b72f18f43
Fernanda Vidal <fevidal.dev@gmail.com>
2022-06-06 20:23:37 -0300

    Corrige path renderWithRouter.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from '../helpers/renderWithRouter';
 import { About } from '../pages';
 
 describe('Verifica o componente About.js', () => {
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 describe('Verifica o componente App.js', () => {
